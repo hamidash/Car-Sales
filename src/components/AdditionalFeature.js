eee8f3904eb6b622d3090aa27108ea3b81701210
Hamida Sh <sharifovahamida@gmail.com>
2021-02-04 00:59:29 -0800

    MVP: Add and Remove new car features

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+
+
 
 const AdditionalFeature = props => {
+
+  const addFeature = (e) => {
+    e.preventDefault();
+    props.addFeature(props.feature);
+  
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick = {addFeature}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
