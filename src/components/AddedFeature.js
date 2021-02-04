9c04492256df2ca96d2f0a7fb138d779d4c21c8f
Hamida Sh <sharifovahamida@gmail.com>
2021-02-03 22:11:41 -0800

    Resetting branch to initial state

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,18 +1,13 @@
 import React from 'react';
-//import { connect } from 'react-redux';
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button" onClick={()=> props.removeFeature(props.feature)}>X</button>
+      <button className="button">X</button>
       {props.feature.name}
     </li>
   );
 };
 
 export default AddedFeature;
-
-//export default connect(null, {})(AddedFeature);
-
-//button className="button" onClick={() => props.removeFeature(props.feature)}>X</button>
--- src/index.js
+++ src/index.js
@@ -5,15 +5,5 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-import {reducer} from './reducer';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-
-const store = createStore(reducer);
-
 const rootElement = document.getElementById('root');
-ReactDOM.render(
-<Provider store = {store}>
-    <App />
-</Provider>
-, rootElement);
+ReactDOM.render(<App />, rootElement);
